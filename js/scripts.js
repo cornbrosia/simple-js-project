b623dac92afa9f2907f1d69e017be377ffacf0f4
rbenwatson <rbenwatson@gmail.com>
2024-04-13 14:12:05 -0400

    Update scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+let pokemonList = [bulbasuar, charmander, squirtle, jigglypuff];
+let bulbasaur = {
+    name: 'Bulbasaur',
+    height: .7,
+    type: ['grass']
+};
+let charmander = {
+    name: 'Charmander',
+    height: .6,
+    type: ['fire']
+};
+let squirtle = {
+    name: 'Squirtle',
+    height: .5,
+    type: ['water']
+};
+let jigglypuff = {
+    name: 'Jigglypuff',
+    height: .5,
+    type: ['fairy', 'normal']
+};
