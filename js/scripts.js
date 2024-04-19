7be1b8e78f89ff4eb7f54fe57068dfadfe233c7b
rbenwatson <rbenwatson@gmail.com>
2024-04-19 16:17:08 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,74 @@
 
-let bulbasaur = {
-    name: 'Bulbasaur',
-    height: .7,
-    type: ['grass']
-};
-let charmander = {
-    name: 'Charmander',
-    height: .6,
-    type: ['fire']
-};
-let squirtle = {
-    name: 'Squirtle',
-    height: .5,
-    type: ['water']
-};
-let jigglypuff = {
-    name: 'Jigglypuff',
-    height: .5,
-    type: ['fairy', 'normal']
-};
-let pokemonList = [bulbasaur, charmander, squirtle, jigglypuff];
+// let bulbasaur = {
+//     name: 'Bulbasaur',
+//     height: .7,
+//     type: ['grass']
+// };
+// let charmander = {
+//     name: 'Charmander',
+//     height: .6,
+//     type: ['fire']
+// };
+// let squirtle = {
+//     name: 'Squirtle',
+//     height: .5,
+//     type: ['water']
+// };
+// let jigglypuff = {
+//     name: 'Jigglypuff',
+//     height: .5,
+//     type: ['fairy', 'normal']
+// };
+// let pokemonList = [bulbasaur, charmander, squirtle, jigglypuff];
+let pokemonRepository = (function () {
+    let bulbasaur = {
+        name: 'Bulbasaur',
+        height: .7,
+        type: ['grass']
+    };
+    let charmander = {
+        name: 'Charmander',
+        height: .6,
+        type: ['fire']
+    };
+    let squirtle = {
+        name: 'Squirtle',
+        height: .5,
+        type: ['water']
+    };
+    let jigglypuff = {
+        name: 'Jigglypuff',
+        height: .5,
+        type: ['fairy', 'normal']
+    };
+    let pokemonList = [bulbasaur, charmander, squirtle, jigglypuff];
+    return {
+        add: function (pokemon) {
+            pokemonList.push(pokemon);
+        },
+        getAll: function () {
+            return pokemonList;
+        }
+    };
+})();
+
+
 //for loop iterating through my array, printing names and checking height
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + ' height:' + pokemonList[i].height); if (pokemonList[i].height >= .6) {
+// for (let i = 0; i < pokemonList.length; i++) {
+//     document.write(pokemonList[i].name + ' height:' + pokemonList[i].height); if (pokemonList[i].height >= .6) {
+//         document.write(' - Wow, thats a big feller!');
+
+//     }
+//     document.write('<br>');
+// }
+
+
+function iterate(pokemonRepository) {
+
+    document.write(pokemonRepository.getAll.name + ' height:' + pokemonRepository.getAll.height); if (pokemonRepository.getAll.height >= .6) {
         document.write(' - Wow, thats a big feller!');
 
     }
     document.write('<br>');
 }
+pokemonRepository.forEach(iterate);
